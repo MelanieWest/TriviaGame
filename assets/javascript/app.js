112a0225506675c026a18b1a7da07278d7781c38
Melanie West <dhmofan@gmail.com>
2017-08-28 16:03:31 -0400

    Add files to repository

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+
+//list groups as a dictionary, with the question/answer pair together
+//keep outside functions so they're global
+
+animalGroups = { 'Alligators':'Congregation' , 'Apes':'Shrewdness', 'Barracudas':'Battery', 'Clams':'Bed',
+'Crows':'Murder', 'Geese':'Gaggle', 'Gnus':'Implausibility', 'Hedgehogs':'Array', 'Ponies':'String', 
+'Mice':'Mischief', 'Mosquitoes':'Scourge', 'Cockroaches':'Intrusion', 'Cobras':'Quiver', 'Owls':'Parliament', 
+'Rattlesnakes':'Rhumba', 'Rhinos':'Crash', 'Salamanders':'Congress', 'Wolves':'Pack', 'Wombats':'Wisdom', 
+'Zebras':'Zeal', 'Lions':'Pride', 'Cats':'Nuisance','Camels':'Caravan'};
+
+
+
+
+$(document).ready(function(){
+
+
+})
