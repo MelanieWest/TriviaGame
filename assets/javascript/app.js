2b061b42372aae5adc19267d7c8220668f0e0be1
Melanie West <dhmofan@gmail.com>
2017-08-30 21:35:21 -0400

    Add images but not yet stats

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,10 @@ var i=0;
 var quiz=[];
 var intervalTimer;
 var delayButtonAlert;
+var Count;
+var imageChoice;
+
+var questionIndex;  //protect answer from changing upon accidental double-click of ans button
 
 quizBuild();        //put data into quiz constructor function to make object
 
@@ -13,34 +17,52 @@ $("#new-question").on("click",function(){       //change from click event to som
 
     $("#message").html('');       
     $("#seconds-count").html("");
+    if(i != 0) {$(".animal").remove();}
 
     quizWrite(i);       //write out i-th question
         
     //begin 1-second-interval timer for display
 
-    intervalTimer = setInterval(myTimer,1000);
-    var Count = 30;  //initialize before starting timer
-
-    function myTimer(){       //this displays the seconds
+    Count = 30;  //initialize before starting timer    
+    intervalTimer = setInterval(function(){
         Count -= 1;
         $("#seconds-count").html('You have '+ Count + " seconds left ")
-    }
-   
+        
+     },1000);
+
+     //begin timeout timer for end-of-question;
+
+     delayButtonAlert = setTimeout(function(){
+    //        $("message").html('5 seconds have passed');
+    //        console.log("image here");
+     //       imageChoice = '<img src=' + 'https://media.giphy.com/media/11nNT2EiGIKsBa/giphy.gif' + '/>';
+            imageChoice = imageInsert(i);
+        $("#picture").append(imageChoice);
+        },5000);  // picture should show up after 5 seconds
+
+
+
+
+
+
     $(".answer").on("click",function(){
         userChoice = parseInt($(this).val());
+        if ((1 <= userChoice) && (userChoice <= 4)){
+            quiz[questionIndex].attempted = true; 
+        }
         console.log(userChoice);
-        console.log(quiz[i]);
-        console.log(quiz[i].ans);
+        console.log(quiz[questionIndex]);
+        console.log(quiz[questionIndex].ans);
         clearTimeout(intervalTimer); 
-        if (userChoice == quiz[i].ans){
-            $("#message").html('correct!'); 
-            i++;      
+        clearTimeout(delayButtonAlert);  
+        if (userChoice == quiz[questionIndex].ans){
+            $("#message").html('correct!');
+  //          i++;
         }
         else {
             $("#message").html('sorry!');
-             i++;       
+    //        i++;
         }
- 
          // increment array item after one attempt (also modify stats here)
        console.log(i);
 });
@@ -49,23 +71,11 @@ $("#new-question").on("click",function(){       //change from click event to som
 
 
 
- //initiate the 30 second timer.  This will execute display of gif, show data and load a new question
+ 
 
- //for testing purposes, make this a shorter time
 
-    $("#new-question").on("click", function(){      //allow answering the question to activate contents also        
-        delayButtonAlert = setTimeout(function(){
-         $("message").html('5 seconds have passed');
-        },5000);
-      });
-
-    //   clearTimeout(delayButtonAlert);        //activate this when I want this to stop/reset
-    $("#stop").on("click",function(){      //for now, start timer will stop with answer or button
-        clearTimeout(startTimer);
-        clearTimeout(delayButtonAlert);    
-    })
     
-
+    i++;
 
 })      //end of 'new question' block
 
@@ -74,7 +84,7 @@ $("#new-question").on("click",function(){       //change from click event to som
 //when first question is loaded, start 2 timers:
 // the seconds count timer and the timeout timer;
 
-function quizConstructor(question,choice1,choice2,choice3,choice4,ans,asked){
+function quizConstructor(question,choice1,choice2,choice3,choice4,ans,imageURL,attempted){
 
     this.question = question;
     this.choice1   = choice1;
@@ -82,7 +92,8 @@ function quizConstructor(question,choice1,choice2,choice3,choice4,ans,asked){
     this.choice3   = choice3;
     this.choice4   = choice4;
     this.ans       = ans;
-    this.asked     = asked;
+    this.imageURL  = imageURL;
+    this.attempted = attempted;
 
 }
 
@@ -90,11 +101,11 @@ function quizBuild(){
 
     console.log('quizBuild');
 
-    quiz[0] = new quizConstructor('Alligators','Scourge','Congregation','Array','Herd',2,false);
-    quiz[1] = new quizConstructor('Crows','Murder','Flutter','Omen','Poe',1,false);
-    quiz[2] = new quizConstructor('Geese','Nuisance','Parliament','Gaggle','Honk',3,false);
-    quiz[3] = new quizConstructor('Mice','Click','Mischief','Nibble','World',2,false);
-    quiz[4] = new quizConstructor('Cockroaches','Immortality','Crunch','Plague','Intrusion',4,false);
+    quiz[0] = new quizConstructor('Alligators','Scourge','Congregation','Array','Herd',2,"https://media.giphy.com/media/11nNT2EiGIKsBa/giphy.gif",false);
+    quiz[1] = new quizConstructor('Crows','Murder','Flutter','Omen','City',1,'https://media.giphy.com/media/VeTm2S9veYuli/giphy.gif',false);
+    quiz[2] = new quizConstructor('Geese','Nuisance','Parliament','Gaggle','Honk',3,'https://media.giphy.com/media/b8hm9N1bjne3S/giphy.gif',false);
+    quiz[3] = new quizConstructor('Mice','Click','Mischief','Nibble','World',2,'https://media.giphy.com/media/tCWMUAuZLMvKg/giphy.gif',false);
+    quiz[4] = new quizConstructor('Cockroaches','Immortality','Crunch','Plague','Intrusion',4,'https://media.giphy.com/media/TYewlOKxANR4s/giphy.gif',false);
     // --- need to modify the rest ---
     // quiz[5] = new quizConstructor('Rhinos','Crash',false;
     // quiz[6] = new quizConstructor('Zebras','Zeal',false);
@@ -123,15 +134,44 @@ function quizWrite(index){
 
     console.log('quizWrite');
 
+    questionIndex = index;
+
     $("#question").html("What do you call a group of " + quiz[index].question + " ?");
     
     $("#option-1").html(quiz[index].choice1);
     $("#option-2").html(quiz[index].choice2);
     $("#option-3").html(quiz[index].choice3);
     $("#option-4").html(quiz[index].choice4);
-    
+
+  return questionIndex;  
 }
 
+//     $('#container').append($('<img>', { 
+//         src : "/path/to/image.jpg", 
+//         width : 16, 
+//         height : 16, 
+//         alt : "Test Image", 
+//         title : "Test Image"
+//     }));
+
+// function reCreateDiv(){
+//     var newDiv = document.createElement("div",{id:"picture"});
+//     var currentDiv = document.getElementById("div1");
+//     document.body.insertBefore(newDiv,currentDiv);
+// }
+
+
+function imageInsert(index){    
+    console.log(quiz[index].imageURL);
+
+    var imageChoice = $('<img>');
+    
+    imageChoice.addClass('animal');
+    
+    imageChoice.attr('src', quiz[index].imageURL);
+                      
+   return imageChoice;
+}
 
 
 
